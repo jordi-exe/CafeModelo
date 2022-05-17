804dda3a71c29579544722034c12895cabf9225a
Jordi <jorrom16@gmail.com>
2022-05-16 21:37:27 -0500

    - AJAX

---
+++ src/main/assets/www/Pages/login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyectoc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$name = $_GET['phpuser'];
+$pass = $_GET['phppass'];
+
+$sql_query = "SELECT count(*) AS cntUser FROM usuarios WHERE Usuario ='$name' AND Contrasena = '$pass'";
+$result = mysqli_query($conn, $sql_query);
+$row = mysqli_fetch_array($result);
+
+$count = $row['cntUser'];
+
+if($count > 0)
+{
+    $_SESSION['name'] = $name;
+    echo 1;
+} else {
+    echo 0;
+}
+
+?>
